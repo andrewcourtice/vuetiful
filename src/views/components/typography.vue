474dbb45444aacea3a2b1e6639537c754a0cb4ac
Andrew Courtice <andrewcourtice@gmail.com>
2017-01-23 22:15:52 +1000

    Little typo change

--- src/views/components/typography.vue
+++ src/views/components/typography.vue
@@ -16,7 +16,7 @@
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <code>function sampleMethod(arg1, arg2) {<br />    var arg3 = arg1 * arg<br /><br />    return arg3;<br />}</code>
+        <code>function sampleMethod(arg1, arg2) {<br />    var arg3 = arg1 * arg2;<br /><br />    return arg3;<br />}</code>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
