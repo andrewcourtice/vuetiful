2f65b236e9bcc23fc1536b78dec453092c312bd6
Andrew Courtice <andrewcourtice@gmail.com>
2017-03-29 11:50:16 +1000

    Adde complex array sort tests

--- test/utilities/sort-by.js
+++ test/utilities/sort-by.js
@@ -1,16 +1,49 @@
-import { assert } from "chai";
+import {
+    assert
+} from "chai";
 import sortBy from "../../src/utilities/sort-by";
 
 const simpleArray = [5, 6, 1, 9, 4];
+
+const complexArray = [
+    {
+        name: "Robert",
+        age: 21
+    },
+    {
+        name: "Cindy",
+        age: 18
+    },
+    {
+        name: "Samantha",
+        age: 35
+    },
+    {
+        name: "Phillip",
+        age: 52
+    },
+    {
+        name: "Scott",
+        age: 23
+    }
+];
 
 const comparitiveOperators = {
     lt: (a, b) => a < b,
     gt: (a, b) => a > b
 };
 
-function verifySortedArray(sortedArray, comparitiveOperator) {
+function verifySortedArray(sortedArray, comparitiveOperator, key) {
     return sortedArray.every((item, index, array) => {
-        return index < array.length - 1 ? comparitiveOperator(array[index], array[index + 1]) : array[index];
+        let valueA = array[index];
+        let valueB = array[index + 1] || valueA;
+
+        if (key) {
+            valueA = valueA[key];
+            valueB = valueB[key];
+        }
+
+        return index < array.length - 1 ? comparitiveOperator(valueA, valueB) : valueA;
     });
 }
 
@@ -28,6 +61,24 @@ describe("Sort By", () => {
         let sortedArray = sortBy(simpleArray, null, -1);
 
         let isSorted = verifySortedArray(sortedArray, comparitiveOperators.gt);
+
+        assert.isTrue(isSorted);
+    });
+
+    it("should sort a complex array in ascending order", () => {
+        const key = "age";
+
+        let sortedArray = sortBy(complexArray, key);
+        let isSorted = verifySortedArray(sortedArray, comparitiveOperators.lt, key);
+
+        assert.isTrue(isSorted);
+    });
+
+    it("should sort a complex array in descending order", () => {
+        const key = "age";
+
+        let sortedArray = sortBy(complexArray, key, -1);
+        let isSorted = verifySortedArray(sortedArray, comparitiveOperators.gt, key);
 
         assert.isTrue(isSorted);
     });
