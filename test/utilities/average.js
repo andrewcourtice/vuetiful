6b981b41a6af52bcc015ac8206ea5f3a3eac688d
Andrew Courtice <andrewcourtice@gmail.com>
2017-03-29 17:29:05 +1000

    Added babel-polyfill to dev dependencies. Updated average test.

--- test/utilities/average.js
+++ test/utilities/average.js
@@ -2,6 +2,7 @@ import {
     assert
 } from "chai";
 import averageOf from "../../src/utilities/average-of";
+import totalOf from "../../src/utilities/total-of";
 
 const array = [5, 6, 5, 3, 9, 4, 7, 1, 4, 9, 0, 3, 5];
 
@@ -9,8 +10,10 @@ describe("Average", () => {
 
     it("should find the average of an array of numeric structures", () => { 
         let average = averageOf(array);
+        let total = totalOf(array);
 
-        assert.isNumber(average);     
+        assert.isNumber(average); 
+        assert.equal(average, total / array.length);    
     });
 
 });
